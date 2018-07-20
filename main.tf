cc4c4a5d20d33a0e79ef81edc2dec24c5e6f7eb7
apdavanzo <39068927+apdavanzo@users.noreply.github.com>
2018-07-20 16:18:38 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,4 +41,22 @@
  *     }
  * 
  */
+     
+module "virtual-machine_example_clone_linux_static" {
+  source               = "vancluever/virtual-machine/vsphere//examples/clone_linux_static"
+  version              = "1.0.1"
 
+  datacenter           = "Datacenter"
+  datastore            = "datastore1"
+  dns_servers          = ["147.75.207.207","147.75.207.208"]
+  domain_name          = "ah-vcsa.hashicorp-success.com"
+  ipv4_address_start   = "1"
+  ipv4_gateway         = "147.75.109.17"
+  ipv4_network_address = "147.75.109.20/29"
+  linked_clone         = "false"
+  network              = "VM Network"
+  resource_pool        = "default-pool"
+  template_name        = "ubuntu"
+  vm_count             = "1"
+  vm_name_prefix       = "test"
+}
